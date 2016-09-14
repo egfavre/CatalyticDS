0f47a37c74b30c92e3bf71f762b4300f7803d548
Liz Favre <egfavre@gmail.com>
2016-09-14 15:56:30 -0400

    Add pages to localhost:8080

--- src/main/java/com/egfavre/controllers/CatalyticDSController.java
+++ src/main/java/com/egfavre/controllers/CatalyticDSController.java
@@ -9,8 +9,12 @@ import com.egfavre.services.PalindromeRepository;
 import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -35,6 +39,26 @@ public class CatalyticDSController {
         public void init() throws SQLException {
             Server.createWebServer().start();
         }
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String home (HttpSession session) {
+        return "home";
+    }
+
+    @RequestMapping (path = "/factor", method = RequestMethod.GET)
+    public String factorPage (HttpSession session, Model model) {
+        return "factor";
+    }
+
+    @RequestMapping (path = "/fibonacci", method = RequestMethod.GET)
+    public String fibonacciPage (HttpSession session, Model model) {
+        return "fibonacci";
+    }
+
+    @RequestMapping (path = "/palindrome", method = RequestMethod.GET)
+    public String palindromePage (HttpSession session, Model model) {
+        return "palindrome";
+    }
 
     //find factors of the input
     public void findFactors (int input){
