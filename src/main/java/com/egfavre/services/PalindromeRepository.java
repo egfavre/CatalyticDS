228a58e6b42d5c45bb8cace7fbe37ca1f8f74887
Liz Favre <egfavre@gmail.com>
2016-09-16 22:05:15 -0400

    Better tests

--- src/main/java/com/egfavre/services/PalindromeRepository.java
+++ src/main/java/com/egfavre/services/PalindromeRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by user on 9/13/16.
  */
 public interface PalindromeRepository extends CrudRepository<Palindrome, Integer> {
+    Iterable<Palindrome> findByInput(String input);
 }
--- src/test/java/com/egfavre/CatalyticDsApplicationTests.java
+++ src/test/java/com/egfavre/CatalyticDsApplicationTests.java
@@ -1,74 +1,17 @@
 package com.egfavre;
 
-import com.egfavre.controllers.CatalyticDSController;
+import com.egfavre.CatalyticDsApplication;
 import com.egfavre.entities.Factor;
+import com.egfavre.entities.Fibonacci;
+import com.egfavre.entities.Palindrome;
 import com.egfavre.services.FactorRepository;
 import com.egfavre.services.FibonacciRepository;
 import com.egfavre.services.PalindromeRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@WebAppConfiguration
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class CatalyticDsApplicationTests {
-	@Autowired
-	FactorRepository factors;
-
-	@Autowired
-	FibonacciRepository fibonaccis;
-
-	@Autowired
-	PalindromeRepository palindromes;
-
-	@Autowired
-	WebApplicationContext wac;
-
-	MockMvc mockMvc;
-
-	@Before
-	public void before() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-	}
-
-
-}
-/*package com.james;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.james.entities.Medication;
-import com.james.entities.ProTip;
-import com.james.entities.Task;
-import com.james.entities.User;
-import com.james.services.MedicationRepository;
-import com.james.services.ProTipsRepository;
-import com.james.services.TaskRepository;
-import com.james.services.UserRepository;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -81,201 +24,115 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+/**
+ * Created by user on 9/16/16.
+ */
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = FamilyFirstApplication.class)
+@SpringApplicationConfiguration(classes = CatalyticDsApplication.class)
 @WebAppConfiguration
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class FamilyFirstApplicationTests {
-
+public class CatalyticDsApplicationTests {
 	@Autowired
 	WebApplicationContext wac;
 
-    @Autowired
-    UserRepository users;
+	@Autowired
+	FactorRepository factors;
 
-    @Autowired
-    TaskRepository tasks;
+	@Autowired
+	FibonacciRepository fibonaccis;
 
-    @Autowired
-    ProTipsRepository tips;
+	@Autowired
+	PalindromeRepository palindromes;
 
-    @Autowired
-    MedicationRepository medications;
+	MockMvc mockMvc;
 
-    MockMvc mockMvc;
-
-    @Before
+	@Before
 	public void before() {
 		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
 	}
 
-	@Test
-	public void aTestLogin() throws Exception {
-        int oldCount = (int) users.count();
-        User user = new User("newtestname", "newtestPassword");
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(user);
-        mockMvc.perform(
-                MockMvcRequestBuilders.post("/login")
-                        .content(json)
-                        .contentType("application/json")
-        );
-        int newCount = (int) users.count();
-
-        Assert.assertTrue(newCount == oldCount + 1);
-    }
-
 
 	@Test
-	public void bTasksGetRouteTest() throws Exception {
-		User testuser = new User("bob", "pass");
-		users.save(testuser);
-		Task task = new Task(testuser, "thing to do", null, null, LocalDateTime.now());
-		tasks.save(task);
+	public void factorPage() throws Exception {
+		Factor factor = new Factor(2, "1,2");
+		factors.save(factor);
 
 		ResultActions ra = mockMvc.perform(
-				MockMvcRequestBuilders.get("/tasks")
+				MockMvcRequestBuilders.get("/factor")
 		);
 		MvcResult result = ra.andReturn();
 		MockHttpServletResponse response = result.getResponse();
 		String json = response.getContentAsString();
 
-        Assert.assertTrue(json.contains("thing to do"));
+		Assert.assertTrue(json.contains("2"));
 	}
 
-    @Test
-    public void caddTaskTest() throws Exception {
-        int oldCount = (int) tasks.count();
-        User taskuser = new User("newtestname", "newtestPassword");
-        String body = new String ("I like peanuts");
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(body);
-        mockMvc.perform(
-                MockMvcRequestBuilders.post("/addTask")
-                        .content(body)
-                        .contentType("application/json")
-        );
-        int newCount = (int) tasks.count();
+	@Test
+	public void fibonacciPage() throws Exception {
+		Fibonacci fibonacci = new Fibonacci(3, 1);
+		fibonaccis.save(fibonacci);
 
-        Assert.assertTrue(newCount == oldCount + 1);
-        Assert.assertTrue(tasks.findFirstByTaskText("I like peanuts") != null);
-    }
+		ResultActions ra = mockMvc.perform(
+				MockMvcRequestBuilders.get("/fibonacci")
+		);
+		MvcResult result = ra.andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		String json = response.getContentAsString();
+
+		Assert.assertTrue(json.contains("3"));
+	}
 
 	@Test
-	public void daddCommentTest() throws Exception {
-		User testuser = users.findFirstByUserName("bob");
-		Task task = new Task(testuser, "thing to do", null, null, LocalDateTime.now());
-		tasks.save(task);
-
-		String comment = "this is the comment";
-
+	public void palindromePage() throws Exception {
+		String input = "race car";
 		mockMvc.perform(
-				MockMvcRequestBuilders.post("/comment" + task.getTaskId())
-							.content(comment)
-							.contentType("application/json")
-			);
+				MockMvcRequestBuilders.post("/palindromeButton")
+						.param("input", input)
+		);
 
-        Task commentedTask = tasks.findOne(task.getTaskId());
+		Assert.assertTrue(palindromes.findByInput("race car") != null);
+	}
 
-        Assert.assertTrue(commentedTask.getCommentText().equals(comment));
-    }
+	@Test
+	public void factorNumber() throws Exception {
 
-    @Test
-    public void ecompleteTest() throws Exception {
-        User testuser = users.findFirstByUserName("bob");
-        Task task = tasks.findFirstByTaskText("thing to do");
+	}
 
-        mockMvc.perform(
-                MockMvcRequestBuilders.post("/complete" + task.getTaskId())
-                    .sessionAttr("userName", "bob")
-        );
+	@Test
+	public void fibonacciNumber() throws Exception {
 
-        Assert.assertTrue(tasks.findOne(task.getTaskId()).getCompletedByUser() != null);
-    }
+	}
 
-    @Test
-    public void fProtipsTest() throws Exception {
-        ResultActions ra = mockMvc.perform(
-                MockMvcRequestBuilders.get("/Protip")
-        );
-        MvcResult result = ra.andReturn();
-        MockHttpServletResponse response = result.getResponse();
-        String json = response.getContentAsString();
+	@Test
+	public void palindromeButton() throws Exception {
+		String input = "race car";
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/palindromeButton")
+						.param("input", input)
+		);
 
-        ObjectMapper om = new ObjectMapper();
-        ProTip pt = om.readValue(json, ProTip.class);
+		Assert.assertTrue(palindromes.findByInput("race car") != null);
+	}
 
-        Assert.assertTrue(pt.getTip().equals(tips.findOne(pt.getTipsId()).getTip()));
-    }
+	@Test
+	public void findFactors() throws Exception {
+	}
 
-    @Test
-    public void gMedsGetTest() throws Exception {
-        User tstUser = new User("MedUser", "testPass");
-        users.save(tstUser);
-        Medication med = new Medication("MedName", "50mg", 4, null, null);
-        medications.save(med);
+	@Test
+	public void findFibonacci() throws Exception {
 
-        ResultActions ra = mockMvc.perform(
-                MockMvcRequestBuilders.get("/meds")
-        );
-        MvcResult result = ra.andReturn();
-        MockHttpServletResponse response = result.getResponse();
-        String json = response.getContentAsString();
+	}
 
-        Assert.assertTrue(json.contains("MedName"));
-    }
+	@Test
+	public void findPalindrome() throws Exception {
 
-    @Test
-    public void hAddMedTest() throws Exception {
-        Medication testMed = new Medication();
-        testMed.setMedName("NewMed");
-        testMed.setDose("30mg");
-        testMed.setFrequency(8);
+	}
 
-        ObjectMapper om = new ObjectMapper();
-        String json = om.writeValueAsString(testMed);
-
-        mockMvc.perform(
-                MockMvcRequestBuilders.post("/addMed")
-                        .content(json)
-                        .contentType("application/json")
-        );
-
-        Assert.assertTrue(medications.count() == 2);
-    }
-
-    @Test
-    public void iGivenMedTest() throws Exception {
-        Medication medInDb = new Medication("TestMed", "10mg", 8, null, null);
-        medications.save(medInDb);
-
-        int i = medications.findByMedName("TestMed").getMedId();
-
-        mockMvc.perform(
-          MockMvcRequestBuilders.post("/given" + i)
-        );
-
-        Assert.assertTrue(medications.findOne(i).getLastGiven() != null);
-    }
-
-    @Test
-    public void jDeleteMedTest() throws Exception {
-
-        int medCount = (int) medications.count();
-
-        int i = medications.findByMedName("TestMed").getMedId();
-
-        mockMvc.perform(
-                MockMvcRequestBuilders.post("/deleteMed" + i)
-        );
-
-        int newMedCount = (int) medications.count();
-
-        Assert.assertTrue(newMedCount == medCount - 1);
-    }
 }
-*/
